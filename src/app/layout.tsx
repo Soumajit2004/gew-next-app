c69a5f9846283e6b20508d326b5444162fc56cc6
Soumajit Ghosh <soumojitghosh02@gmail.com>
2022-12-11 08:25:39 +0530

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/globals.css';
+
+export default function RootLayout({
+                                       children,
+                                   } :
+                                       {
+                                           children: React.ReactNode;
+                                       }) {
+    return (
+        <html lang="en">
+        <body>
+        <main>
+            {children}
+        </main>
+        </body>
+        </html>
+    );
+}
